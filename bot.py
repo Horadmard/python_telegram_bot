1b1d9b21f836f21225a448e9b53abfc009b7a9a7
Horad <iranradmard1@gmail.com>
2024-07-17 13:08:30 +0330

    commit

--- bot.py
+++ bot.py
@@ -27,6 +27,32 @@ async def dec_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 def handle_response(text: str) -> str:
 
+    # print('What do you wanna do?\
+    # 1. Encrypt Text into an Image\
+    # 2. Decrypte an Image and reach to a Text\
+    # 3. Do nothing, Thanks for nothing!')
+
+    # inputs: list = text.split()
+    choice, plain_text, image_name = text.split()
+
+    # choice = int(input())
+
+    if int(choice) == 1:
+        # enc_command()
+        # plain_text = input('Your massage:')
+        # image_name = input('Image name:')
+        encrypt.text_to_image(plain_text, image_name)
+
+    elif choice == 2:
+        image_name = input('Image name:')
+        decrypt.image_to_text(image_name)
+
+    # elif choice == 3:
+    #     break
+
+    else:
+        print('Invalid!')
+
 
 
 
